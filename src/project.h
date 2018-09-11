8caf8a73cc18709c5ac631aab13466ad76755cd9
YanaGofarova <https://github.com/YanaGofarova>
2018-09-11 07:12:29 +0700

    Add project.h

---
+++ src/project.h
@@ -0,0 +1,11 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+#define _CRT_SECURE_NO_WARNINGS
+//void clrscr();
+void clrbrd();
+void print_board();
+int get_move();
+int wonQ(char player);
+char get_winner();
+
+#endif	
